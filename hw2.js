7fcd9e62e0010da3efde165cb498b1e9de50665c
Asia Osimeh <osimeha1@southernct.edu>
2024-09-24 02:30:31 +0000

    HW2

---
+++ hw2.js
@@ -0,0 +1,30 @@
+1) highlighted in light orange (list of linux commands) vv
+
+
+"ls" lists all items inside the directory
+"pwd" (print working directory) displays the full pathname of the current directory in use
+"cd" is used to change between different directories
+"mkdir" creates a directory
+"cat" (concatenate) shows all content in the pico files without opening pico itself
+"ssh" (secure shell) is used for logging in the VM
+
+
+2) highlighted in yellow (list of git commands) vv
+
+
+"git config" configures options for github
+"git clone" clones/ copies the repository from github to personal PC
+"git status" is used to check the status of local repository
+"git add" is used to add items into the git hub repository
+"git commit" to upload any changes made to github
+"git push" is used to push any changes in the local repository
+
+3) i. Include the collaborations in a separate document (if you collab with anyone).
+        Otherwise, put "I did not collaborate with anyone while working on this assignment".
+
+   ii.  Include external sources in a separate document. Otherwise, put "I did not consult any sources other than
+        the textbook while working on this assignment".
+
+   iii. Penalty of 10% daily for late assignments
+
+   iv.  Loss of information and suffers an attendance penalty, which is about 5%
